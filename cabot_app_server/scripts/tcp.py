d74f7492c297590f7b14b3d90ec72ef9a6173173
Takashi Itoh <takashi.ito@jst.go.jp>
2025-12-12 14:54:44 +0900

    Enable WebUI only with env CABOT_USE_WEBUI=1
    Signed-off-by: Takashi Itoh <takashi.ito@jst.go.jp>

--- cabot_app_server/scripts/tcp.py
+++ cabot_app_server/scripts/tcp.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 import json
+import os
 import time
 import traceback
 from flask import Flask
@@ -173,7 +174,8 @@ class CaBotTCP():
             # wait while heart beat is valid
             self.last_heartbeat = time.time()
             common.logger.info("CaBotTCP listening...")
-            self.webui = __import__("webui").WebUI(self)
+            if os.getenv('CABOT_USE_WEBUI') == '1':
+                self.webui = __import__("webui").WebUI(self)
             self.app.run(host='0.0.0.0', port=5000)
 
         except (KeyboardInterrupt, SystemExit):
